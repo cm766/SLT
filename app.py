a2581ff8af775c6bfac27b56d8aada91de609dd5
cm766 <80293205+cm766@users.noreply.github.com>
2023-12-28 15:27:04 +0000

    improving layout

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def generate_frames():
 @app.route('/')
 def index():
     return render_template('index.html')
-
+          
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
